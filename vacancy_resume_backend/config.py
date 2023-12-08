71f623a5b57d8fdbb8a0b0e5dac603cae46931ee
Sasha <sasha_petr04@mail.ru>
2023-12-08 20:06:34 +0300

    version 1.8

--- vacancy_resume_backend/config.py
+++ vacancy_resume_backend/config.py
@@ -8,3 +8,5 @@ COLLECTION_NAME = os.environ.get("COLLECTION_NAME")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 HOST = os.environ.get("HOST")
 PORT = os.environ.get("PORT")
+
+print(CONNECTION_STRING)
