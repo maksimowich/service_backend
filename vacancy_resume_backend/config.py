62f436e3ab8f8b9bd96bc2dbcd612ba55d374df1
Александра Петрова <99252008+sashapetr04@users.noreply.github.com>
2024-02-18 14:41:03 +0300

    Create config.py

---
+++ vacancy_resume_backend/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+STORAGE_PATH = os.environ.get("STORAGE_PATH")
